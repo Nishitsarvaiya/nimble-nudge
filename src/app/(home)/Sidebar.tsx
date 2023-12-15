60500b31b2b5bff570855de043dab31490a3f4f5
Nishit Sarvaiya <nishitsarvaiiya@gmail.com>
2023-12-15 09:05:29 +0530

    added icons

--- src/app/(home)/Sidebar.tsx
+++ src/app/(home)/Sidebar.tsx
@@ -1,17 +1,62 @@
-import Image from "next/image";
-import React from "react";
+'use client';
+import { Icons } from '@/components/Icons';
+import { Button } from '@/components/ui/button';
+import { Switch } from '@/components/ui/switch';
+import { cn } from '@/lib/utils';
+import Link from 'next/link';
+import { useState } from 'react';
 
-type Props = {};
-
-export default function Sidebar({}: Props) {
+export default function Sidebar() {
+	const [isLightTheme, setIsLightTheme] = useState<boolean>(false);
 	return (
-		<aside className="fixed h-full top-0 left-0 w-[100px]">
-			<div className="flex flex-col h-full">
-				<div className="flex flex-col items-center py-8 gap-8">
-					<div className="relative w-10 h-10">
-						<Image src="/logo-dark.svg" alt="NN" fill />
+		<aside className='fixed h-full top-0 left-0 w-[100px] border-r border-r-gray'>
+			<div className='flex flex-col items-center justify-between h-full'>
+				<div className='flex flex-col items-center py-10 gap-10'>
+					<Link href='/'>
+						<Icons.logoDark className='w-10 h-10 fill-primary' />
+					</Link>
+					<div className='w-12 h-[1px] bg-gray'></div>
+					<div className='flex flex-col items-center gap-1'>
+						<Button asChild variant='ghost' size='icon'>
+							<Link href='/'>
+								<Icons.messages className='w-8 h-8 fill-primary' />
+							</Link>
+						</Button>
+						<Button asChild variant='ghost' size='icon'>
+							<Link href='/people'>
+								<Icons.users className='w-8 h-8 fill-primary' />
+							</Link>
+						</Button>
+						<Button asChild variant='ghost' size='icon'>
+							<Link href='/add-friend'>
+								<Icons.addUser className='w-8 h-8 fill-primary' />
+							</Link>
+						</Button>
+						<Button asChild variant='ghost' size='icon'>
+							<Link href='/notifications'>
+								<Icons.bell className='w-8 h-8 fill-primary' />
+							</Link>
+						</Button>
 					</div>
-					<div className="w-12 h-[1px] bg-gray"></div>
+				</div>
+				<div className='flex flex-col items-center py-10 gap-10'>
+					<div className='rotate-90'>
+						<Switch
+							id='theme-switcher'
+							className='relative'
+							checked={isLightTheme}
+							onCheckedChange={(checked) => setIsLightTheme(checked)}
+						>
+							<div className='h-full w-full flex items-center justify-center absolute'>
+								<div className='h-full flex-1 flex items-center justify-center ml-[-2px]'>
+									<Icons.sun className='fill-primary h-5 w-5' />
+								</div>
+								<div className='h-full flex-1 flex items-center justify-center mr-[-2px]'>
+									<Icons.moon className='fill-primary h-5 w-5 -rotate-90' />
+								</div>
+							</div>
+						</Switch>
+					</div>
 				</div>
 			</div>
 		</aside>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,62 +1,64 @@
-import type { Config } from "tailwindcss";
-import { fontFamily } from "tailwindcss/defaultTheme";
+import type { Config } from 'tailwindcss';
+import { fontFamily } from 'tailwindcss/defaultTheme';
 
 const config: Config = {
-	darkMode: ["class"],
+	darkMode: ['class'],
 	content: [
-		"./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+		'./src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/app/**/*.{js,ts,jsx,tsx,mdx}',
 	],
 	theme: {
 		extend: {
 			fontFamily: {
-				sans: ["var(--primary-font)", ...fontFamily.sans],
+				sans: ['var(--primary-font)', ...fontFamily.sans],
 			},
 			colors: {
-				border: "hsl(var(--border))",
-				input: "hsl(var(--input))",
-				ring: "hsl(var(--ring))",
-				background: "hsl(var(--background))",
-				foreground: "hsl(var(--foreground))",
+				border: 'hsl(var(--border))',
+				input: 'hsl(var(--input))',
+				ring: 'hsl(var(--ring))',
+				background: 'hsl(var(--background))',
+				foreground: 'hsl(var(--foreground))',
 				chat: {
-					DEFAULT: "hsl(var(--chat))",
-					foreground: "hsl(var(--chat-foreground))",
+					DEFAULT: 'hsl(var(--chat))',
+					foreground: 'hsl(var(--chat-foreground))',
 				},
 				gray: {
-					DEFAULT: "hsl(var(--gray))",
+					DEFAULT: 'hsl(var(--gray))',
+					500: 'hsl(var(--gray-light))',
+					900: 'hsl(var(--gray-text))',
 				},
 				primary: {
-					DEFAULT: "hsl(var(--primary))",
-					foreground: "hsl(var(--primary-foreground))",
+					DEFAULT: 'hsl(var(--primary))',
+					foreground: 'hsl(var(--primary-foreground))',
 				},
 				secondary: {
-					DEFAULT: "hsl(var(--secondary))",
-					foreground: "hsl(var(--secondary-foreground))",
+					DEFAULT: 'hsl(var(--secondary))',
+					foreground: 'hsl(var(--secondary-foreground))',
 				},
 				destructive: {
-					DEFAULT: "hsl(var(--destructive))",
-					foreground: "hsl(var(--destructive-foreground))",
+					DEFAULT: 'hsl(var(--destructive))',
+					foreground: 'hsl(var(--destructive-foreground))',
 				},
 				muted: {
-					DEFAULT: "hsl(var(--muted))",
-					foreground: "hsl(var(--muted-foreground))",
+					DEFAULT: 'hsl(var(--muted))',
+					foreground: 'hsl(var(--muted-foreground))',
 				},
 				accent: {
-					DEFAULT: "hsl(var(--accent))",
-					foreground: "hsl(var(--accent-foreground))",
+					DEFAULT: 'hsl(var(--accent))',
+					foreground: 'hsl(var(--accent-foreground))',
 				},
 				popover: {
-					DEFAULT: "hsl(var(--popover))",
-					foreground: "hsl(var(--popover-foreground))",
+					DEFAULT: 'hsl(var(--popover))',
+					foreground: 'hsl(var(--popover-foreground))',
 				},
 				card: {
-					DEFAULT: "hsl(var(--card))",
-					foreground: "hsl(var(--card-foreground))",
+					DEFAULT: 'hsl(var(--card))',
+					foreground: 'hsl(var(--card-foreground))',
 				},
 			},
 		},
 	},
-	plugins: [require("tailwindcss-animate")],
+	plugins: [require('tailwindcss-animate')],
 };
 export default config;
